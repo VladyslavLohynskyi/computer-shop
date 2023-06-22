7ac510780a1af38efdbf4a3040ea20f1c965fd50
VladyslavLohynskyi <watcher.good2003@gmail.com>
2023-06-22 21:04:57 +0300

    added models for db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
-
+const models = require("./models/models");
+const cors = require("cors");
 const PORT = process.env.PORT || 5050;
 const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Working" });
+});
 const start = async () => {
   try {
     await sequelize.authenticate();
