bc268d5af1b17533618b747465766b95c5ab78ee
VladyslavLohynskyi <watcher.good2003@gmail.com>
2023-06-22 21:52:42 +0300

    added typeControler(create, getAll functionaly)

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,7 +1,7 @@
 const Router = require("express");
 const router = new Router();
-
-router.post("/");
-router.get("/");
+const TypeController = require("../controllers/typeController");
+router.post("/", TypeController.create);
+router.get("/", TypeController.getAll);
 
 module.exports = router;
