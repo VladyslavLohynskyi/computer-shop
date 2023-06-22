506f88efcaee44d3cc51e990715b2f56c2520bd2
VladyslavLohynskyi <watcher.good2003@gmail.com>
2023-06-22 22:52:59 +0300

    added checkRoleMiddleware to check permissions

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ const models = require("./models/models");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
+const path = require("path");
 const PORT = process.env.PORT || 5050;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
 app.use(fileUpload({}));
 app.use("/api", router);
 
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,7 +1,8 @@
 const Router = require("express");
 const router = new Router();
 const TypeController = require("../controllers/typeController");
-router.post("/", TypeController.create);
+const checkRole = require("../middleware/checkRoleMiddleware");
+router.post("/", checkRole("ADMIN"), TypeController.create);
 router.get("/", TypeController.getAll);
 
 module.exports = router;
